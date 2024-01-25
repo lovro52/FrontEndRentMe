d1fee360d006dd445b62a1c228a55ac42169d74d
Lovro Luka Matan <lmatan@student.unipu.hr>
2024-01-25 12:16:11 +0100

    Login/register

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,45 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Nekretnine4U</v-toolbar-title>
       <v-spacer></v-spacer>
-
+      <v-btn color="white" rounded class="my-2" text to="/">Home</v-btn>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        color="white"
+        rounded
+        class="my-2"
+        v-if="!currentUser"
         text
+        to="/login"
+        >Login</v-btn
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-btn
+        color="white"
+        rounded
+        class="my-2"
+        v-if="!currentUser"
+        text
+        to="/register"
+        >Register</v-btn
+      >
+      <v-btn color="white" rounded class="my-2" text to="/profile"
+        >Profile</v-btn
+      >
+      <v-btn color="white" rounded class="my-2" text to="/Nekretnine"
+        >Nekretnine</v-btn
+      >
+      <v-menu bottom min-width="200px" rounded offset-y> </v-menu>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
